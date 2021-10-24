8014e8019937677eb4939780429d30378f87c517
Erwin Janssen <erwinjanssen@outlook.com>
2021-10-24 11:54:13 +0200

    Use importlib_metadata for Python < 3.8
    The module `import.metadata` was added in Python 3.8. For earlier
    version, the package `importlib_metadata` can be used.

--- {{cookiecutter.project_slug}}/src/{{cookiecutter.module_slug}}/__init__.py
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.module_slug}}/__init__.py
@@ -1,4 +1,11 @@
-import importlib.metadata
+# The module `import.metadata` was added in Python 3.8. For earlier version,
+# the package `importlib_metadata` can be used.
+try:
+    import importlib.metadata
+except ModuleNotFoundError:
+    import importlib
+    import importlib_metadata
+    importlib.metadata = importlib_metadata
 
 # Single source version number by using the metadata from the installed
 # package.
