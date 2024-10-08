2694fa73ae548ef89ac4d3a47d134e53b40f9f35
Erwin Janssen <erwinjanssen@outlook.com>
2024-10-08 20:45:50 +0200

    Assume at least Python 3.9
    Python 3.8 is end-of-life, and can be removed from the tox envlist. It
    can also be assumed that `importlib.metadata` is available.

--- {{cookiecutter.project_slug}}/src/{{cookiecutter.module_slug}}/__init__.py
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.module_slug}}/__init__.py
@@ -1,11 +1,4 @@
-# The module `import.metadata` was added in Python 3.8. For earlier version,
-# the package `importlib_metadata` can be used.
-try:
-    import importlib.metadata
-except ModuleNotFoundError:
-    import importlib
-    import importlib_metadata
-    importlib.metadata = importlib_metadata
+import importlib.metadata
 
 # Single source version number by using the metadata from the installed
 # package.
