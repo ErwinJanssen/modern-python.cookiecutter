93f12a570a3a8881ca9b7ef7a50a1d2e41da957e
Erwin Janssen <erwinjanssen@outlook.com>
2024-10-08 21:15:55 +0200

    Create initial commit and tag after project create

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -17,6 +17,27 @@ def initialize_pre_commit():
     )
 
 
+def first_commit():
+    subprocess.run(
+        ["git", "add", "."], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+    )
+    proc = subprocess.run(
+        ["git", "commit", "--message", "Initialized project"],
+        check=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
+    print(proc.stderr)
+    proc.check_returncode()
+    subprocess.run(
+        ["git", "tag", "--annotate", "--message", "Initialized project", "v0.0.0"],
+        check=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
+
+
 if __name__ == "__main__":
     initialize_git()
     initialize_pre_commit()
+    first_commit()
